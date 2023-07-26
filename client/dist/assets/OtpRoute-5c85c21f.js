39b24f4f78c6579761827ce8dcb08dda38532e79
Musthafa7232 <musthafa723262@gmail.com>
2023-07-26 20:48:53 +0530

    production build

--- client/dist/assets/OtpRoute-5c85c21f.js
+++
@@ -1,1 +0,0 @@
-import{p as o,j as t,$ as a,a0 as s}from"./index-28413705.js";function u(){return o(e=>e.phone).number?t.jsx(a,{}):t.jsx(s,{to:"/login"})}export{u as default};
---
+++ client/dist/assets/OtpRoute-c3629be4.js
@@ -0,0 +1,1 @@
+import{p as o,j as t,$ as a,a0 as s}from"./index-d1f1fba9.js";function u(){return o(e=>e.phone).number?t.jsx(a,{}):t.jsx(s,{to:"/login"})}export{u as default};
