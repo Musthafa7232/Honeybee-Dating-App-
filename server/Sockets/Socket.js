5e5097378b807640c1f1e4bf19ddea2a7fc0ab05
Musthafa7232 <musthafa723262@gmail.com>
2023-07-29 14:59:13 +0530

    production commit

--- server/Sockets/Socket.js
+++ server/Sockets/Socket.js
@@ -6,7 +6,7 @@ import { isUserMatched } from "../usecases/MatchesInteractor.js";
 import { disconnect } from "mongoose";
 const io = new Server({
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://honeyhub.zodiacwatches.shop",
   },
   pingTimeout: 60000,
 });
