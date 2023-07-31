6737c77cc2b40ce45455288ad031bae93fbea091
Musthafa7232 <musthafa723262@gmail.com>
2023-07-31 16:35:19 +0530

    added cors

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(cors({ origin: 'https://honeybee.zodiacwatches.shop' }));
 app.use(express.json());
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: false }));
